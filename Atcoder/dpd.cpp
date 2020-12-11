6f99040b2a92d0f5b5db7d6a807bc4d85146208d
Max Jiang <maxjiang123@gmail.com>
2020-12-10 21:32:39 -0500

    Update dpd.cpp

--- Atcoder/dpd.cpp
+++ Atcoder/dpd.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
-ll dp[100001];
+const int mod=1e9+7, seed=131, MAXN=101, MAXV=1e3+1;
+
+ll dp[MAXN*MAXV], tot;
 
 int main(){
-    int items,value; cin >> items >> value;
-    
+    cin.tie(0)->sync_with_stdio(0);
+    int n, w; cin >> n >> w;
+    ms(dp, 0x3f); dp[0]=0;
+    for (int i=1;i<=n;++i){
+        ll w, v; cin >> w >> v;
+        tot+=v;
+        for (int j=tot;j>=v;--j)
+            dp[j]=min(dp[j],dp[j-v]+w);
+    }
+    for (int i=tot;i>=0;--i){
+        if (dp[i]<=w){ cout << i << '\n'; break; }
+    }
 }
