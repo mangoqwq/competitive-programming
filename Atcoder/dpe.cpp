d22b94a8e93e8df762d6b1d86a3141f71cdcbb23
emuyumi <maxjiang123@gmail.com>
2020-12-11 21:36:26 -0500

    Update dpe.cpp

--- Atcoder/dpe.cpp
+++ Atcoder/dpe.cpp
@@ -1,39 +1,23 @@
-//Knapsack 2
 #include <bits/stdc++.h>
-#include <vector>
 
 using namespace std;
+#define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
+const int mod=1e9+7, seed=131, MAXN=101, MAXV=1e3+1;
+
+ll dp[MAXN*MAXV], tot;
 
 int main(){
-    int N,W,a,b;
-    cin >> N >> W;
-    pair<ll,int> items[N];
-    for (int i=0;i<N;++i){
-        cin >> a >> b;
-        items[i].first=a; items[i].second=b;
+    cin.tie(0)->sync_with_stdio(0);
+    int n, w; cin >> n >> w;
+    ms(dp, 0x3f); dp[0]=0;
+    for (int i=1;i<=n;++i){
+        ll w, v; cin >> w >> v;
+        tot+=v;
+        for (int j=tot;j>=v;--j)
+            dp[j]=min(dp[j],dp[j-v]+w);
     }
-    ll dp[(1000*N)+1];
-    fill(dp, dp+(1000*N)+1,1000000001);
-    for (int item=0;item<N;++item){
-        for (int i=(1000*N);i>=0;--i){
-            if (items[item].second<i){
-                dp[i]=min(dp[i-items[item].second]+items[item].first,dp[i]);
-            }
-            else if (items[item].second==i){
-                dp[i]=min(dp[i],items[item].first);
-            }
-        }
+    for (int i=tot;i>=0;--i){
+        if (dp[i]<=w){ cout << i << '\n'; break; }
     }
-    for (int i=(1000*N);i>=0;--i){
-        if (dp[i]<=W){
-            cout << i << '\n';
-            break;
-        }
-    }
-    
-
-
-
-
 }
