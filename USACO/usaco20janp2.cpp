9ffd8cca72e63800294c9310543c4cc164441048
emuyumi <maxjiang123@gmail.com>
2021-01-15 08:56:30 -0500

    Update usaco20janp2.cpp

--- USACO/usaco20janp2.cpp
+++ USACO/usaco20janp2.cpp
@@ -1,67 +1,59 @@
 #include <bits/stdc++.h>
-//#pragma GCC optimize ("Ofast")
 
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
-typedef pair<int,int> pii;
-const int mod=1e9+7,seed=131,MAXN=5e4+1,MAXQ=2e5+1;
+const int mod=1e9+7, seed=131, MAXN=5e4+5, MAXQ=2e5+1;
 
-int arr[MAXN]; int lc[MAXN+1][21],rc[MAXN+1][21],cnt[21];
-struct Query{ int l,r,id; };
-int ans[MAXQ];
+int a[MAXN]; ll ans[MAXQ];
+ll cl[MAXN][21], cr[MAXN][21];
+struct Query{ int l, r, id; };
 
-void solve(int l, int r, vector<Query> &q){
-    if (l>r || q.empty()) return;
-    ll tot; int mid=(l+r)/2;
-    for (int j=1;j<=20;++j){
-        ms(cnt,0); tot=0;
+void solve(int l, int r, vector<Query> qs){
+    int mid=l+r>>1;
+    vector<Query> ql, qr, q;
+    for (Query x:qs){
+        if (x.r<mid) ql.push_back(x);
+        else if (x.l>mid) qr.push_back(x);
+        else q.push_back(x);
+    }
+    for (int k=0;k<=20;++k){
+        ms(cl[mid+1],0), ms(cr[mid],0);
+        if (k==0) cl[mid+1][0]=1;
         for (int i=mid;i>=l;--i){
-            for (int k=arr[i];k<=j;++k){
-                tot+=cnt[k]; cnt[arr[i]]+=cnt[k]; 
-                cnt[arr[i]]%=mod; tot%=mod;
-            }
-            if (arr[i]==j) cnt[j]++, tot++;
-            lc[i][j]=tot;
+            for (int j=0;j<=20;++j) cl[i][j]=cl[i+1][j];
+            if (a[i]==k) cl[i][a[i]]++;
+            for (int j=a[i];j<=20;++j) cl[i][a[i]]+=cl[i+1][j], cl[i][a[i]]%=mod;
         }
-        ms(cnt,0); tot=0;
         for (int i=mid+1;i<=r;++i){
-            for (int k=arr[i];k>=j;--k){
-                tot+=cnt[k]; cnt[arr[i]]+=cnt[k];
-                cnt[arr[i]]%=mod; tot%=mod;
-            }
-            if (arr[i]==j) cnt[j]++, tot++;
-            rc[i][j]=tot;
+            for (int j=0;j<=20;++j) cr[i][j]=cr[i-1][j];
+            if (a[i]<k) continue;
+            cr[i][a[i]]++;
+            for (int j=a[i];j>=0;--j) cr[i][a[i]]+=cr[i-1][j], cr[i][a[i]]%=mod;
+        }
+        for (int i=l;i<=r;++i){
+            cl[i][0]=accumulate(cl[i],cl[i]+21,0LL)%mod;
+            cr[i][0]=accumulate(cr[i],cr[i]+21,0LL)%mod;
+        }
+        for (Query x:q){
+            ans[x.id]+=1LL*cl[x.l][0]*(cr[x.r][0]+1);
+            ans[x.id]%=mod;
         }
     }
-    ll temp,uwu;
-    vector<Query> left, right;
-    for (Query x:q){
-        if (x.r<mid){ left.push_back(x); continue; }
-        if (x.l>mid){ right.push_back(x); continue; }
-        temp=1;
-        for (int i=1;i<=20;++i) temp+=rc[x.r][i], temp%=mod;
-        uwu=temp;
-        for (int i=1;i<=20;++i){
-            uwu=(uwu+(temp*lc[x.l][i])%mod)%mod;
-            temp=(temp-rc[x.r][i]+mod)%mod;
-        }
-        ans[x.id]=uwu;
-    }
-    solve(1,mid-1,left); solve(mid+1,r,right);
+    if (!ql.empty()) solve(l,mid-1,ql);
+    if (!qr.empty()) solve(mid+1,r,qr);
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    freopen("name.in","r",stdin);
-    freopen("name.out","w",stdout);
-    int n,k; cin >> n >> k;
-    for (int i=1;i<=n;++i) cin >> arr[i];
-    int q; cin >> q;
-    vector<Query> uwu;
-    for (int i=1,a,b;i<=q;++i) cin >> a >> b, uwu.push_back({a,b,i});
-    // for (int i=144;i<=152;++i) cout << arr[i] << " ";
-    // cout << '\n';
-    solve(1,n,uwu);
-    for (int i=1;i<=q;++i) cout << ans[i] << '\n';
+    int n, k, q; cin >> n >> k;
+    for (int i=1;i<=n;++i) cin >> a[i];
+    cin >> q;
+    vector<Query> qs(q);
+    for (int i=0;i<q;++i){
+        cin >> qs[i].l >> qs[i].r;
+        qs[i].id=i;
+    }
+    solve(1,n,qs);
+    for (int i=0;i<q;++i) cout << ans[i] << '\n';
 }
