3ab00401fd9d8993b0e500da2080eeac5fa6554d
emuyumi <maxjiang123@gmail.com>
2021-01-17 16:49:40 -0500

    Add dmopc19c7p5.cpp

--- DMOPC/dmopc19c7p5.cpp
+++ DMOPC/dmopc19c7p5.cpp
@@ -1,81 +1,57 @@
 #include <bits/stdc++.h>
-//#pragma GCC optimize ("Ofast,unroll-loops")
 
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
-#define f first
-#define s second
 typedef long long ll;
-typedef pair<int,int> pii;
-const int mod=1e9+7,seed=131,MAXN=500'001;
+const int mod=1e9+7, seed=131, MAXN=5e5+1;
 
-int bit[MAXN],arr[MAXN];
-pii tree[MAXN*4];
-
-int query(int x){
-    int ans=0;
-    for (int i=x;i>0;i-=i&-i){
-        ans+=bit[i];
+struct Fenwick{
+    int a[MAXN];
+    void ins(int i, int val=1){ for (;i<MAXN;i+=i&-i) a[i]+=val; }
+    int qry(int i){
+        int ret=0;
+        for (;i;i-=i&-i) ret+=a[i];
+        return ret;
     }
-    return ans;
-}
+} bit1, bit2;
+struct Query{ int i, id, nxt; } q[MAXN], x;
+int arr[MAXN]; ll ans[MAXN];
 
-void update(int x){
-    for (int i=x;i<=MAXN;i+=i&-i){
-        bit[i]++;
+void solve(int l, int r, int head){
+    if (!head||l==r) return;
+    int mid=l+r>>1, cnt=0;
+    int sl=0, sr=0, hl=0, hr=0;
+    for (int i=head;i;i=q[i].nxt){
+        x=q[i];
+        if (arr[x.i]>mid){
+            bit1.ins(x.i);
+            ans[x.id]+=cnt-bit2.qry(x.i);
+            if (sr==0) sr=i;
+            q[hr].nxt=i;
+            hr=i;
+        }
+        else{
+            bit2.ins(x.i); cnt++;
+            ans[x.id]+=bit1.qry(x.i);
+            if (sl==0) sl=i;
+            q[hl].nxt=i;
+            hl=i;
+        }
     }
-}
-
-int segquery(int node, int start, int end, int l,int r,int val){
-    if (start>r||end<l) return 0;
-    if (start>=l&&end<=r){
-        if (val==1) return tree[node].first;
-        if (val==2) return tree[node].second;
-    }
-    int mid=(start+end)/2;
-    return segquery(node*2,start,mid,l,r,val)+segquery(node*2+1,mid+1,end,l,r,val);
-}
-
-void segupdate(int node, int start, int end, int index, int val){
-    if (start==end&&start==index){
-        tree[node]={val==1,val==2};
-        return;
-    }
-    int mid=(start+end)/2;
-    if (index<=mid) segupdate(node*2,start,mid,index,val);
-    else segupdate(node*2+1,mid+1,end,index,val);
-    tree[node]={tree[node*2].f+tree[node*2+1].f,tree[node*2].s+tree[node*2+1].s};
+    q[hl].nxt=q[hr].nxt=0;
+    for (int i=sr;i;i=q[i].nxt) bit1.ins(q[i].i,-1);
+    for (int i=sl;i;i=q[i].nxt) bit2.ins(q[i].i,-1);
+    solve(l,mid,sl), solve(mid+1,r,sr);
 }
 
 int main(){
-    cin.tie(0); cin.sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
     int n; cin >> n;
-    bool subtask2=true;
+    for (int i=1;i<=n;++i) cin >> arr[i];
     for (int i=1;i<=n;++i){
-        cin >> arr[i];
-        subtask2&=(arr[i]==1||arr[i]==2);
+        cin >> q[i].i;
+        q[i].id=i, q[i-1].nxt=i;
     }
-    if (!subtask2){
-        int q; ll cnt=0;
-        for (int i=1;i<=n;++i){
-            cin >> q;
-            cnt+=i-query(arr[q])-1;
-            update(arr[q]);
-            cout << cnt << '\n';
-        }
-    }
-    else{
-        int q; ll cnt=0;
-        for (int i=1;i<=n;++i){
-            cin >> q;
-            if (arr[q]==1){
-                cnt+=segquery(1,1,n,1,q,2);
-            }
-            if (arr[q]==2){
-                cnt+=segquery(1,1,n,q,n,1);
-            }
-            segupdate(1,1,n,q,arr[q]);
-            cout << cnt << '\n';
-        }
-    }
+    solve(1,n,1);
+    for (int i=1;i<=n;++i) cout << (ans[i]+=ans[i-1]) << '\n';
 }
