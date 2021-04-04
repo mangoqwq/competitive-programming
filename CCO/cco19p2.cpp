343a89e2b83026d4466962024e7ef407ba3cde08
emuyumi <maxjiang123@gmail.com>
2021-04-04 10:35:54 -0400

    Add cco19p2.cpp

--- CCO/cco19p2.cpp
+++ CCO/cco19p2.cpp
@@ -1,114 +1,81 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int,int> pii;
-vector<vector<pair<char,int>>> graph;
-int visited[500005];
-vector<pii> blocks[500005];
-int lowest[1000001];
+#define ms(x,a) memset(x,a,sizeof x)
+typedef long long ll;
+const int mod=1e9+7, inf=0x3f3f3f3f, MAXN=1e6+1;
 
-void bfs(int x, int y, int group){
-    if (!graph[x][y].second){
-        graph[x][y].second=group;
-        blocks[group].push_back({x,y});
-        if (graph[x+1][y].first=='#'){
-            bfs(x+1,y,group);
-        }
-        if (graph[x-1][y].first=='#'){
-            bfs(x-1,y,group);
-        }
-        if (graph[x][y+1].first=='#'){
-            bfs(x,y+1,group);
-        }
-        if (graph[x][y-1].first=='#'){
-            bfs(x,y-1,group);
-        }
+struct Coord{ int x, y; };
+queue<Coord> q;
+struct Edge{
+    int v, w;
+    bool operator<(const Edge &x) const{
+        return w>x.w;
     }
-}
+};
+priority_queue<Edge> pq;
+vector<Edge> adj[MAXN];
+int dist[MAXN];
 
 int main(){
-    int rows,columns;
-    cin >> rows >> columns;
-    graph.resize(rows+2);
-    for (int i=0;i<rows+2;++i){
-        graph[i].resize(columns+2);
+    // cin.tie(0)->sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    int g[n+1][m+1], id[n+1][m+1], ans[n+1][m+1], T=0;
+    ms(g,0), ms(id,0), ms(ans,0);
+    for (int i=1;i<=n;++i) for (int j=1;j<=m;++j){
+        char a; cin >> a;
+        g[i][j]=a=='#';
     }
-    for (int x=1;x<rows+1;++x){
-        for (int y=1;y<columns+1;++y){
-            cin >> graph[x][y].first;
-        }
-    }
-    reverse(graph.begin(),graph.end());
-    int group=1;
-    for (int x=1;x<rows+1;++x){
-        for (int y=1;y<columns+1;++y){
-            if (graph[x][y].first=='#'&&!graph[x][y].second){
-                bfs(x,y,group);
-                group++;
+    for (int i=1;i<=n;++i){
+        for (int j=1;j<=m;++j){
+            if (!g[i][j]||id[i][j]) continue;
+            id[i][j]=++T;
+            q.push({i,j});
+            while (!q.empty()){
+                auto [x,y]=q.front(); q.pop();
+                for (int dx=-1;dx<=1;++dx){
+                    for (int dy=-1;dy<=1;++dy){
+                        if (abs(dx+dy)!=1) continue;
+                        if (x+dx<1||x+dx>n||y+dy<1||y+dy>m) continue;
+                        if (g[x+dx][y+dy]&&!id[x+dx][y+dy]){
+                            id[x+dx][y+dy]=T;
+                            q.push({x+dx,y+dy});
+                        }
+                    }
+                }
             }
         }
     }
-    //first is height, second is channel
-    visited[0]=1;
-    int falldist=0x3f3f3f3f,block,temp,tempheight;
-    for (int x=1;x<rows+1;++x){
-        for (int y=1;y<columns+1;++y){
-            if (graph[x][y].first=='#'&&!visited[graph[x][y].second]){
-                falldist=0x3f3f3f3f;
-                block=graph[x][y].second;
-                visited[block]=1;
-                for (auto&&uwu:blocks[block]){
-                    if (uwu.first-lowest[uwu.second]>=1&&false){
-                        falldist=min(falldist,uwu.first-lowest[uwu.second]);
-                    }
-                    else{
-                        tempheight=uwu.first-1;
-                        while (tempheight>0&&graph[tempheight][uwu.second].first!='#'&&graph[tempheight][uwu.second].second!=block){
-                            tempheight--;
-                        }
-                        if (uwu.first-tempheight>=1){
-                            falldist=min(falldist,uwu.first-tempheight);
-                        }
-                        
-                    }
-                }
-                falldist--;
-                cout << "~" << falldist << "~"<<block << " ";
-                //if (falldist<0){
-                //    continue;
-                //}
-                for (int i=0;i<blocks[block].size();++i){
-                    graph[blocks[block][i].first][blocks[block][i].second]={'.',0};
-                    blocks[block][i].first-=falldist;
-                    lowest[blocks[block][i].second]=max(lowest[blocks[block][i].second],blocks[block][i].first);
-                    graph[blocks[block][i].first][blocks[block][i].second]={'#',group};
-                    //cout << blocks[block][i].first << " " << blocks[block][i].second << "|";
-                }
-                //cout << '\n';
-                
+    for (int j=1;j<=m;++j){
+        int pre=0, lastc=n+1;
+        for (int i=n;i>=1;--i){
+            if (!id[i][j]) continue;
+            if (id[i][j]!=pre){
+                adj[pre].push_back({id[i][j],lastc-i-1});
+                pre=id[i][j];
+            }
+            lastc=i;
+        }
+    }
+    ms(dist,0x3f);
+    dist[0]=0;
+    pq.push({0,0});
+    while (!pq.empty()){
+        auto [v,d]=pq.top(); pq.pop();
+        for (auto [to,w]:adj[v]){
+            if (dist[to]>d+w){
+                dist[to]=d+w;
+                pq.push({to,dist[to]});
             }
         }
     }
-    cout << '\n';
-    char uwu[rows+1][columns+1];
-    for (int x=0;x<rows+1;++x){
-        for (int y=0;y<columns+1;++y){
-            uwu[x][y]='.';
-        }
+    for (int i=1;i<=n;++i) for (int j=1;j<=m;++j){
+        if (id[i][j]) ans[i+dist[id[i][j]]][j]=1;
     }
-    for (auto&&x:blocks){
-        for (auto&&owo:x){
-            uwu[owo.first][owo.second]='#';
-        }
-    }
-    for (int x=rows;x>0;--x){
-        for (int y=1;y<columns+1;++y){
-            cout << uwu[x][y];
+    for (int i=1;i<=n;++i){
+        for (int j=1;j<=m;++j){
+            cout << (ans[i][j]?'#':'.');
         }
         cout << '\n';
     }
-
-    
-
-
 }
