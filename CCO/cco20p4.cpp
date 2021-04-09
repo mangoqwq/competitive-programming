b194ccf50e7e664fda99b9d618a5105de045018f
emuyumi <maxjiang123@gmail.com>
2021-04-09 19:00:11 -0400

    Add cco20p4.cpp

--- CCO/cco20p4.cpp
+++ CCO/cco20p4.cpp
@@ -1,103 +1,55 @@
 #include <bits/stdc++.h>
-//#pragma GCC optimize ("Ofast,unroll-loops")
 
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
-typedef pair<int,int> pii;
-const int mod=1e9+7,seed=131,MAXN=2001;
+const int mod=1e9+7, inf=0x3f3f3f3f, MAXN=2001;
 
-struct Edge{int a,b;};
-vector<int> graph[MAXN];
-int p[MAXN],sz[MAXN];
-vector<Edge> red;
-vector<Edge> blue;
-int heavy[MAXN];
-bool isred[MAXN];
-
-int Find(int x){ return (p[x]==x)?x:Find(p[x]); }
-void Union(int a,int b){
-    a=Find(a); b=Find(b);
-    if (sz[b]>sz[a]) swap(a,b);
-    p[b]=a; sz[a]+=sz[b];
-}
-
-int find_heavy(int curr, int prev){
-    pii temp={1,-1};
-    for (int x:graph[curr]){
-        if (x==prev) continue;
-        temp=max(temp,make_pair(find_heavy(x,curr),x));
-    }
-    heavy[curr]=temp.second;
-    return temp.first+1;
-}
-
-void dfs(int curr, int prev,vector<int> &ans){
-    ans.push_back(curr);
-    for (int x:graph[curr]){
-        if (x==prev||x==heavy[curr]) continue;
-        dfs(x,curr,ans);
-        ans.push_back(curr);
-    }
-    if (heavy[curr]!=-1){
-        dfs(heavy[curr],curr,ans);
-        ans.push_back(curr);
-    }
-}
+bool adj[MAXN][MAXN];
+int lst[MAXN], nxt[MAXN];
 
 int main(){
-    cin.tie(0); cin.sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
     int n; cin >> n;
-    char temp;
-    for (int i=1;i<=n;++i){
-        sz[i]=1; p[i]=i;
-        for (int k=i-1;k>=1;--k){
-            cin >> temp;
-            if (temp=='R') red.push_back({i,k});
-            else blue.push_back({i,k});
+    for (int i=2;i<=n;++i){
+        for (int j=1;j<i;++j){
+            char a; cin >> a;
+            adj[i][j]=adj[j][i]=a=='B';
         }
     }
-    for (Edge x:red){
-        if (Find(x.a)==Find(x.b)) continue;
-        Union(x.a,x.b);
-        //cout << x.a << " " << x.b << '\n';
-        isred[x.a]=1;
-        isred[x.b]=1;
-        graph[x.a].push_back(x.b);
-        graph[x.b].push_back(x.a);
-    }
-    for (int i=1;i<=n;++i){
-        if (Find(i)!=Find(1)) p[i]=i,sz[i]=1,graph[i].clear();
-    }
-    bool merged=0;
-    for (Edge x:blue){
-        if (Find(x.a)==Find(x.b)) continue;
-        if (isred[x.a]||isred[x.b]){
-            if (merged) continue;
-            else merged=1;
+    for (int src=1;src<=n;++src){
+        ms(lst,0), ms(nxt,0);
+        int m=src, p=src;
+        for (int i=1;i<=n;++i){
+            if (i==src) continue;
+            if (m==src||m==p){
+                int a=p;
+                lst[i]=a; nxt[a]=i;
+                if (adj[i][a]==adj[lst[a]][a]) m=i;
+                else m=a;
+                p=i;
+            }
+            else{
+                if (adj[i][m]==adj[m][lst[m]]){
+                    int a=m, b=nxt[m];
+                    if (adj[i][a]==adj[i][b]) m=b;
+                    else m=i;
+                    nxt[a]=i, lst[b]=i;
+                    lst[i]=a, nxt[i]=b;
+                }
+                else{
+                    int a=lst[m], b=m;
+                    if (adj[i][a]==adj[i][b]) m=a;
+                    else m=i;
+                    nxt[a]=i, lst[b]=i;
+                    lst[i]=a, nxt[i]=b;
+                }
+            }
         }
-        Union(x.a,x.b);
-        //cout << x.a << " " << x.b << '\n';
-        graph[x.a].push_back(x.b);
-        graph[x.b].push_back(x.a);
-    }
-    for (int i=1;i<=n;++i){
-        if (Find(i)!=Find(1)) assert(false);
-    }
-    int prev;
-    ms(heavy,-1);
-    for (int i=1;i<=n;++i){
-        find_heavy(i,i);
-        vector<int> ans;
-        set<int> s;
-        dfs(i,i,ans);
-        while (!s.count(ans.back())) prev=ans.back(),s.insert(prev),ans.pop_back();
-        cout << ans.size()+1 << '\n';
-        for (int x:ans){
-            cout << x << " ";
+        cout << n << '\n';
+        for (int i=src;i;i=nxt[i]){
+            cout << i << " ";
         }
-        cout << prev << '\n';
+        cout << '\n';
     }
-
-
 }
