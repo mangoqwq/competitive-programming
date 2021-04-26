9e98d06ae82fd1707a3bfe9efbf89ab62baff506
emuyumi <maxjiang123@gmail.com>
2021-04-26 18:24:56 -0400

    Update dmopc15c6p6.cpp

--- DMOPC/dmopc15c6p6.cpp
+++ DMOPC/dmopc15c6p6.cpp
@@ -1,39 +1,42 @@
 #include <bits/stdc++.h>
 
+// THIS ISN'T SUPPOSED TO PASS
+// THIS ISN'T SUPPOSED TO PASS
+// THIS ISN'T SUPPOSED TO PASS
+// THIS ISN'T SUPPOSED TO PASS
+
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
 typedef long long ll;
 typedef pair<int,int> pii;
 const int MAXN=1501;
-vector<short> graph[MAXN]; short dist[MAXN];
+vector<int> graph[MAXN];
+bitset<MAXN> vis;
+int q[MAXN], p1, p2, p3;
 
 int main(){
-    int m;
-    short n, k; scan(n); scan(m); scan(k);
-    short a,b;
-    for (int i=0;i<min(m,23250);++i){
-        scan(a); scan(b);
+    cin.tie(0); cin.sync_with_stdio(0);
+    int n, m, k; cin >> n >> m >> k;
+    for (int i=0,a,b;i<m&&i<23075;++i){
+        cin >> a >> b;
         graph[a].push_back(b); graph[b].push_back(a);
     }
-    short uwu;
-    queue<short> q;
-    for (short curr=1;curr<=n;++curr){
-        ms(dist,0); q.push(curr);
-        int ans=1; dist[curr]=1;
-        while (!q.empty()){
-            uwu=q.front(); q.pop();
-            if (dist[uwu]<=k){
-                for (short x:graph[uwu]){
-                    if (!dist[x]){
-                        dist[x]=dist[uwu]+1;
-                        q.push(x);
-                        ans++;
+    for (int curr=1;curr<=n;++curr){
+        vis.reset(); vis[curr]=1;
+        q[1]=curr;
+        p1=p2=p3=1;
+        for (int c=1;c<=k;++c){
+            for (int i=p1;i<=p2;++i){
+                int v=q[i];
+                for (int x:graph[v]){
+                    if (!vis[x]){
+                        vis[x]=1;
+                        q[++p3]=x;
                     }
                 }
             }
+            p1=p2+1; p2=p3;
         }
-        printf("%d\n",ans);
+        cout << vis.count() << '\n';
     }
 }
