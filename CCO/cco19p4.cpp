5fb1360c0d821805bc541e826fb92a9f9d19153e
emuyumi <maxjiang123@gmail.com>
2021-05-09 22:53:18 -0400

    Add cco19p4.cpp

--- CCO/cco19p4.cpp
+++ CCO/cco19p4.cpp
@@ -1,26 +1,50 @@
 #include <bits/stdc++.h>
-//#pragma GCC optimize ("Ofast")
 
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
+#define mid ((l+r)>>1)
 typedef long long ll;
-typedef pair<int,int> pii;
-const int mod=1e9+7,seed=131,MAXN=1e6+1;
+const int mod=1e9+7, inf=0x3f3f3f3f, MAXN=1e6+1;
 
-int arr[MAXN], cnt[MAXN], best;
-double ans, dp[MAXN];
+int k, n;
+struct Function{
+    ll c; double d;
+    double operator()(ll x){
+        if (x<c) return d;
+        return pow(sqrt(x-c),k)+d;
+    }
+};
+struct Node{
+    Node *lc=0, *rc=0;
+    Function f={-1,-1e12};
+    void extendl(){ if (!lc) lc=new Node(); }
+    void extendr(){ if (!rc) rc=new Node(); }
+    void ins(Function x, int l=1, int r=n){
+        bool lo=x(l)>f(l);
+        bool mi=x(mid)>f(mid);
+        if (mi) swap(f,x);
+        if (l==r||(!lc&&!rc&&f.c==-1)) return;
+        if (lo!=mi) extendl(), lc->ins(x,l,mid);
+        else extendr(), rc->ins(x,mid+1,r);
+    }
+    double qry(int i, int l=1, int r=n){
+        if (l==r||(!lc&&!rc)) return f(i);
+        if (i<=mid) return extendl(), max(f(i),lc->qry(i,l,mid));
+        else return extendr(), max(f(i),rc->qry(i,mid+1,r));
+    }
+} *lct[MAXN];
+int c[MAXN]; double dp[MAXN];
+vector<Function> uwu[MAXN];
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    int k,n; cin >> k >> n;
-    for (int i=1;i<=n;++i) cin >> arr[i];
+    cin >> k >> n;
     for (int i=1;i<=n;++i){
-        for (int j=i;j>0;--j){
-            cnt[arr[j]]++;
-            if (cnt[arr[j]]>cnt[best]) best=arr[j];
-            dp[i]=max(dp[i],dp[j-1]+pow(sqrt(cnt[best]),k));
-        }
-        for (int j=i;j>0;--j) cnt[arr[j]]=0;
+        int x; cin >> x;
+        c[x]++;
+        if (!lct[x]) lct[x]=new Node();
+        lct[x]->ins({c[x]-1,dp[i-1]});
+        dp[i]=lct[x]->qry(c[x]);
     }
-    cout << setprecision(15) << dp[n] << '\n';
+    cout << fixed << setprecision(14) << dp[n] << '\n';
 }
