ff030d976b954bd702e769c489589ceb33e0f18e
emuyumi <maxjiang123@gmail.com>
2021-07-11 14:01:46 -0400

    Update dmopc17c1p6.cpp

--- DMOPC/dmopc17c1p6.cpp
+++ DMOPC/dmopc17c1p6.cpp
@@ -3,84 +3,100 @@
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
-const int mod=1e9+7, seed=131, MAXN=1e5+1;
+const int mod=1e9+7, inf=0x3f3f3f3f, MAXN=1e5+1, SZ=1000;
 
-struct nd{
-    nd *c[2]={0}, *p={0};
-    int val, tot;
-    bool lz=0;
-    nd(int x){ val=tot=x; }
-    void push(){
-        if (lz){
-            swap(c[0],c[1]);
-            if (c[0]) c[0]->lz^=1;
-            if (c[1]) c[1]->lz^=1;
-            lz=0;
+int n, q; 
+struct Query{ char op; int x, y, z=0; };
+vector<Query> qs[MAXN];
+
+int blk[MAXN];
+unordered_map<int,int> adj[MAXN], tmp[MAXN];
+unordered_map<int,pair<int,int>> g[MAXN];
+int rt[MAXN], dist[MAXN];
+
+namespace Rebuild{
+    void dfs(int v, int p, int d, int src){
+        rt[v]=src, dist[v]=d;
+        for (auto [to,w]:tmp[v]){
+            if (to==p) continue;
+            dfs(to,v,d^w,src);
         }
     }
-} *lct[MAXN];
-map<pair<nd*,nd*>,nd*> adj;
-
-int tot(nd *t){ return t?t->tot:0; }
-void upd(nd *t){ t->tot=t->val^tot(t->c[0])^tot(t->c[1]); }
-bool nrt(nd *t){ return t->p&&(t->p->c[0]==t||t->p->c[1]==t); }
-
-void rot(nd *t){
-    bool dir=t->p->c[0]==t;
-    nd *p=t->p;
-    if ((p->c[!dir]=t->c[dir])) t->c[dir]->p=p;
-    if ((t->p=p->p)&&nrt(p)) p->p->c[p->p->c[1]==p]=t;
-    t->c[dir]=p, p->p=t;
-    upd(p), upd(t);
-}
-
-void splay(nd *t){
-    assert(t);
-    while (nrt(t)){
-        if (nrt(t->p)) t->p->p->push();
-        t->p->push(), t->push();
-        if (nrt(t->p)){
-            bool dir=t->p->p->c[1]==t->p;
-            if (t->p->c[dir]==t) rot(t->p);
-            else rot(t);
-        } rot(t);
+    void go(vector<Query> &qs){
+        for (int i=1;i<=n;++i){
+            rt[i]=0, dist[i]=0;
+            g[i].clear();
+            tmp[i]=adj[i];
+            g[i].clear();
+        }
+        for (auto [op,x,y,z]:qs){
+            if (op=='R'&&adj[x].count(y)){
+                tmp[x].erase(y);
+                tmp[y].erase(x);
+            }
+        }
+        for (int i=1;i<=n;++i) if (!rt[i]) dfs(i,i,0,i);
+        for (auto [op,x,y,z]:qs){
+            if (op=='R'&&adj[x].count(y)){
+                g[rt[x]][y]={adj[x][y],x};
+                g[rt[y]][x]={adj[y][x],y};
+            }
+        }
     }
-    t->push(); upd(t);
 }
 
-void access(nd *t){ for (nd *last=0, *v=t;v||(splay(t),0);splay(v), v->c[1]=last, last=v, v=v->p); }
-void evert(nd *a){ access(a), a->lz^=1; }
-nd *path(nd *a, nd *b){ return evert(a), access(b), b; }
-void link(nd *a, nd* b){ evert(b), b->p=a; }
-void cut(nd *a){ access(a); if (a->c[0]) a->c[0]->p=0; a->c[0]=0; upd(a); }
-bool connected(nd *a, nd *b){ access(a), access(b); return a->p||a==b; }
-
-void add(nd *a, nd *b, int c){
-    nd *t=new nd(c);
-    adj[{a,b}]=t;
-    link(a,t), link(b,t);
-}
-
-void rem(nd *a, nd *b){
-    nd *t=adj[{a,b}]; adj.erase({a,b});
-    evert(a); cut(b); cut(t); 
+namespace LCT{
+    int ans;
+    void link(int x, int y, int z){
+        adj[x][y]=z;
+        adj[y][x]=z;
+        g[rt[x]][y]={adj[x][y],x};
+        g[rt[y]][x]={adj[y][x],y};
+    }
+    void cut(int x, int y){
+        adj[x].erase(y);
+        adj[y].erase(x);
+        g[rt[x]].erase(y);
+        g[rt[y]].erase(x);
+    }
+    void tree(int v, int p, int d, int t){
+        if (rt[v]==rt[t]) ans=d^dist[v]^dist[t];
+        for (auto [to,pii]:g[rt[v]]){
+            auto [w,u]=pii;
+            if (to==p) continue;
+            tree(to,u,d^dist[v]^dist[u]^w,t);
+        }
+    }
+    void go(vector<Query> &qs){
+        for (auto [op,x,y,z]:qs){
+            if (op=='Q') ans=-1;
+            if (op=='A') link(x,y,z);
+            if (op=='R') cut(x,y);
+            if (op=='Q') tree(x,x,0,y);
+            if (op=='Q') cout << ans << '\n';
+        }
+    }
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    int n, q, a, b, c; cin >> n >> q;
-    for (int i=1;i<=n;++i) lct[i]=new nd(0);
+    cin >> n >> q;
+    for (int i=1;i<=q;++i) blk[i]=i/SZ;
+    vector<Query> graph;
     for (int i=1;i<=n-1;++i){
-        cin >> a >> b >> c;
-        if (b>a) swap(a,b);
-        add(lct[a],lct[b],c);
+        int x, y, z; cin >> x >> y >> z;
+        graph.push_back({'A',x,y,z});
+    } 
+    LCT::go(graph);
+    for (int i=1;i<=q;++i){
+        char op; cin >> op;
+        int x, y, z=0;
+        if (op=='A') cin >> x >> y >> z;
+        else cin >> x >> y;
+        qs[blk[i]].push_back({op,x,y,z});
     }
-    char op;
-    while (q--){
-        cin >> op >> a >> b;
-        if (b>a) swap(a,b);
-        if (op=='A') cin >> c, add(lct[a],lct[b],c);
-        if (op=='R') rem(lct[a],lct[b]);
-        if (op=='Q') cout << (connected(lct[a],lct[b])?path(lct[a],lct[b])->tot:-1) << '\n';
+    for (int i=0;i*SZ<=q;++i){
+        Rebuild::go(qs[i]);
+        LCT::go(qs[i]);
     }
 }
