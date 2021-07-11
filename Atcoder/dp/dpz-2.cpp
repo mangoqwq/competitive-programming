ffb1258ab324a03d7fbd8ff0ff0a713edd845ccc
emuyumi <maxjiang123@gmail.com>
2021-07-11 13:59:38 -0400

    Add dpz-2.cpp

--- Atcoder/dp/dpz-2.cpp
+++ Atcoder/dp/dpz-2.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 #define ms(x,a) memset(x,a,sizeof x)
 typedef long long ll;
-const int mod=1e9+7,seed=131,MAXN=2e5+1;
+const int mod=1e9+7, inf=0x3f3f3f3f, MAXN=2e5+1;
 
-ll h[MAXN], dp[MAXN];
+int n;
+ll h[MAXN], dp[MAXN], c;
 deque<int> q;
-
-double f(int j, int k){ // k is better than j when <= ret
-    return (double)(h[k]*h[k]+dp[k]-h[j]*h[j]-dp[j])/(2.0*(h[k]-h[j]));
+double slope(int k, int j){
+    return (h[j]*h[j]-h[k]*h[k]+dp[j]-dp[k])/(2.0*(h[j]-h[k]));
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    int n; ll c; cin >> n >> c;
-    for (int i=1;i<=n;++i) cin >> h[i];y
-    dp[1]=0; q.push_back(1);
+    cin >> n >> c;
+    cin >> h[1]; q.push_front(1);
     for (int i=2;i<=n;++i){
-        while (q.size()>=2 && f(q[0],q[1])<=h[i]) q.pop_front();
-        dp[i]=dp[q[0]]+(h[q[0]]-h[i])*(h[q[0]]-h[i])+c;
-        while (q.size()>=2 && f(q[q.size()-2],q.back())>=f(q.back(),i)) q.pop_back();
-        q.push_back(i);
+        cin >> h[i];
+        while (q.size()>=2&&slope(q[q.size()-1],q[q.size()-2])<h[i]) q.pop_back();
+        int j=q.back();
+        dp[i]=(h[i]-h[j])*(h[i]-h[j])+dp[j]+c;
+        while (q.size()>=2&&slope(q[1],q[0])>slope(q[0],i)) q.pop_front();
+        q.push_front(i);
     }
     cout << dp[n] << '\n';
 }
