72e4f2eab6b65d935b254f40300d037e34711f78
Max Jiang <maxjiang123@gmail.com>
2023-05-16 15:27:24 -0400

    Update cses1707.cpp

--- CSES/cses1707.cpp
+++ CSES/cses1707.cpp
@@ -1,34 +1,59 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
-int main(){
-    cin.tie(0)->sync_with_stdio(0);
-    int N, M; cin >> N >> M;
-    vector<vector<int>> adj(N + 1);
-    for (int i = 1; i <= M; ++i){
-        int a, b; cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    int ans = 1e9;
-    vector<int> dist(N + 1);
-    function<void(int, int)> dfs = [&](int v, int p){
-        for (int to : adj[v]){
-            if (to == p) continue;
-            if (dist[to]){
-                ans = min(ans, abs(dist[v] - dist[to]) + 1);
-            }
-            else{
-                dist[to] = dist[v] + 1;
-                dfs(to, v);
-            }
-        }
-    };
-    for (int i = 1; i <= N; ++i){
-        dist = vector<int>(N + 1, 0);
-        dist[i] = 1;
-        dfs(i, i);
-    }
-    cout << (ans == 1e9 ? -1 : ans) << '\n';
+#ifdef _DEBUG
+#include "utils/debug.h"
+#else
+#define debug(...)
+#endif
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, M; cin >> N >> M;
+
+	vector<int> ea(M), eb(M);
+	struct Edge{ int to, id; };
+	vector<vector<Edge>> adj(N);
+	int ans = 1e9;
+	for (int i = 0; i < M; ++i) {
+		int a, b; cin >> a >> b;
+		a--, b--;
+		ea[i] = a, eb[i] = b;
+		if (a == b) ans = 1;
+		adj[a].push_back({b, i});
+		adj[b].push_back({a, i});
+	}
+
+	auto attempt = [&](int rt) -> void {
+		vector<int> dist(N, 1e9);
+		vector<int> used(M, 0);
+
+		queue<int> q;
+		q.push(rt);
+		dist[rt] = 0;
+		while (!q.empty()) {
+			int v = q.front(); q.pop();
+			for (auto [to, id] : adj[v]) {
+				if (dist[to] == 1e9) {
+					used[id] = 1;
+					dist[to] = dist[v] + 1;
+					q.push(to);
+				}
+			}
+		}
+
+		for (int i = 0; i < M; ++i) {
+			if (used[i]) continue;
+			ans = min(ans, dist[ea[i]] + dist[eb[i]] + 1);
+		}
+	};
+
+	for (int i = 0; i < N; ++i) {
+		attempt(i);
+	}
+	if (ans == 1e9) cout << -1 << '\n';
+	else cout << ans << '\n';
+
 }
